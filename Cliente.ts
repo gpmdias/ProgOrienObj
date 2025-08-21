62a52a7ff39db9f0d2395e50284dc263e4a2ea2f
GUILHERME PEREIRA MAGLIA DIAS <05515242009@senacrs.edu.br>
2025-08-21 11:31:17 -0300

    Adiciona construtor

--- Cliente.ts
+++ Cliente.ts
@@ -1,4 +1,12 @@
 export class Cliente{
      nome: string = "";
      telefone: string = "";
+
+
+     // Método especial que executa apenas uma vez, que é ao criar o objeto 
+     
+     constructor(nomeCliente: string) {
+          this.nome = nomeCliente;
+          this.telefone = "";
+     }
 }   
--- Main.ts
+++ Main.ts
@@ -1,3 +1,5 @@
 import { Cliente } from "./Cliente";
 
-const cli: Cliente = new Cliente()
+const cli: Cliente = new Cliente("Gladimir");
+cli.telefone = "1234-5678";
+console.table(cli);
